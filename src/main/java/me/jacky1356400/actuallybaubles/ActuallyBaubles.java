4cd94c275cab2f8e68a0ce7d3a89b5d5a86ebc69
Jacky1356400 <jacky1356400@hotmail.com>
2017-08-01 05:00:53 +0800

    A few changes
    - Fixed recipes because I am stupid
    - Added batteries in bauble
    - Shoutout to TheRealp455w0rd for helping me out with this update

--- src/main/java/me/jacky1356400/actuallybaubles/ActuallyBaubles.java
+++ src/main/java/me/jacky1356400/actuallybaubles/ActuallyBaubles.java
@@ -1,6 +1,7 @@
 package me.jacky1356400.actuallybaubles;
 
 import de.ellpeck.actuallyadditions.mod.items.InitItems;
+import me.jacky1356400.actuallybaubles.item.ItemBatteryBauble;
 import me.jacky1356400.actuallybaubles.item.ItemMagnetRingBauble;
 import me.jacky1356400.actuallybaubles.item.ItemPotionRingAdvancedBauble;
 import net.minecraft.client.Minecraft;
@@ -43,30 +44,48 @@ public class ActuallyBaubles {
             return "Actually Baubles";
         }
     };
-    public static ItemMagnetRingBauble magnetRing;
-    public static ItemPotionRingAdvancedBauble potionRingAdvanced;
+    public static ItemMagnetRingBauble magnetRing = new ItemMagnetRingBauble("magnet_ring_bauble");
+    public static ItemPotionRingAdvancedBauble potionRingAdvanced = new ItemPotionRingAdvancedBauble(true, "potion_ring_advanced_bauble");
+    public static ItemBatteryBauble battery = new ItemBatteryBauble("battery_bauble", 200000, 1000);
+    public static ItemBatteryBauble batteryDouble = new ItemBatteryBauble("battery_double_bauble", 350000, 5000);
+    public static ItemBatteryBauble batteryTriple = new ItemBatteryBauble("battery_triple_bauble", 600000, 10000);
+    public static ItemBatteryBauble batteryQuadruple = new ItemBatteryBauble("battery_quadruple_bauble", 1000000, 30000);
+    public static ItemBatteryBauble batteryQuintuple = new ItemBatteryBauble("battery_quintuple_bauble", 2000000, 100000);
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent e){
+    public void preInit(FMLPreInitializationEvent event){
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
     public void onItemRegistry(RegistryEvent.Register<Item> event) {
-        ITEMS.add(new ItemMagnetRingBauble("magnet_ring_bauble"));
-        ITEMS.add(new ItemPotionRingAdvancedBauble(true, "potion_ring_advanced_bauble"));
+        ITEMS.add(magnetRing);
+        ITEMS.add(potionRingAdvanced);
+        ITEMS.add(battery.setUnlocalizedName("actuallyadditions.item_battery"));
+        ITEMS.add(batteryDouble.setUnlocalizedName("actuallyadditions.item_battery_double"));
+        ITEMS.add(batteryTriple.setUnlocalizedName("actuallyadditions.item_battery_triple"));
+        ITEMS.add(batteryQuadruple.setUnlocalizedName("actuallyadditions.item_battery_quadruple"));
+        ITEMS.add(batteryQuintuple.setUnlocalizedName("actuallyadditions.item_battery_quintuple"));
         event.getRegistry().registerAll(ITEMS.toArray(new Item[0]));
     }
 
     @SubscribeEvent
     public void onRecipeRegistry(RegistryEvent.Register<IRecipe> event) {
-        Item magnetRingOld = Item.getByNameOrId("actuallyadditions:item_suction_ring");
-        Item potionRingOld = Item.getByNameOrId("actuallyadditions:item_potion_ring_advanced");
-        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "magnet_ring_to_bauble"), new ResourceLocation(MODID, "magnet_ring_to_bauble"), new ItemStack(magnetRing, 1), new Ingredient[]{Ingredient.fromStacks(new ItemStack[]{new ItemStack(magnetRingOld, 1)})});
-        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_magnet_ring"), new ResourceLocation(MODID, "bauble_to_magnet_ring"), new ItemStack(magnetRingOld, 1), new Ingredient[]{Ingredient.fromStacks(new ItemStack[]{new ItemStack(magnetRing, 1)})});
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "magnet_ring_to_bauble"), new ResourceLocation(MODID, "magnet_ring_to_bauble"), new ItemStack(magnetRing, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemMagnetRing, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_magnet_ring"), new ResourceLocation(MODID, "bauble_to_magnet_ring"), new ItemStack(InitItems.itemMagnetRing, 1), Ingredient.fromStacks(new ItemStack(magnetRing, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "battery_to_bauble_1"), new ResourceLocation(MODID, "battery_to_bauble_1"), new ItemStack(battery, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemBattery, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "battery_to_bauble_2"), new ResourceLocation(MODID, "battery_to_bauble_2"), new ItemStack(batteryDouble, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemBatteryDouble, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "battery_to_bauble_3"), new ResourceLocation(MODID, "battery_to_bauble_3"), new ItemStack(batteryTriple, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemBatteryTriple, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "battery_to_bauble_4"), new ResourceLocation(MODID, "battery_to_bauble_4"), new ItemStack(batteryQuadruple, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemBatteryQuadruple, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "battery_to_bauble_5"), new ResourceLocation(MODID, "battery_to_bauble_5"), new ItemStack(batteryQuintuple, 1), Ingredient.fromStacks(new ItemStack(InitItems.itemBatteryQuintuple, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_battery_1"), new ResourceLocation(MODID, "bauble_to_battery_1"), new ItemStack(InitItems.itemBattery, 1), Ingredient.fromStacks(new ItemStack(battery, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_battery_2"), new ResourceLocation(MODID, "bauble_to_battery_2"), new ItemStack(InitItems.itemBatteryDouble, 1), Ingredient.fromStacks(new ItemStack(batteryDouble, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_battery_3"), new ResourceLocation(MODID, "bauble_to_battery_3"), new ItemStack(InitItems.itemBatteryTriple, 1), Ingredient.fromStacks(new ItemStack(batteryTriple, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_battery_4"), new ResourceLocation(MODID, "bauble_to_battery_4"), new ItemStack(InitItems.itemBatteryQuadruple, 1), Ingredient.fromStacks(new ItemStack(batteryQuadruple, 1)));
+        GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_battery_5"), new ResourceLocation(MODID, "bauble_to_battery_5"), new ItemStack(InitItems.itemBatteryQuintuple, 1), Ingredient.fromStacks(new ItemStack(batteryQuintuple, 1)));
         for (int i = 0; i < ALL_RINGS.length; i++) {
-            GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "potion_ring_to_bauble_" + i), new ResourceLocation(MODID, "potion_ring_to_bauble_" + i), new ItemStack(potionRingAdvanced, 1, i), new Ingredient[]{Ingredient.fromStacks(new ItemStack[]{new ItemStack(potionRingOld, 1, i)})});
-            GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_potion_ring_" + i), new ResourceLocation(MODID, "bauble_to_potion_ring_" + i), new ItemStack(potionRingOld, 1, i), new Ingredient[]{Ingredient.fromStacks(new ItemStack[]{new ItemStack(potionRingAdvanced, 1, i)})});
+            GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "potion_ring_to_bauble_" + i), new ResourceLocation(MODID, "potion_ring_to_bauble_" + i), new ItemStack(potionRingAdvanced, 1, i), Ingredient.fromStacks(new ItemStack(InitItems.itemPotionRingAdvanced, 1, i)));
+            GameRegistry.addShapelessRecipe(new ResourceLocation(MODID, "bauble_to_potion_ring_" + i), new ResourceLocation(MODID, "bauble_to_potion_ring_" + i), new ItemStack(InitItems.itemPotionRingAdvanced, 1, i), Ingredient.fromStacks(new ItemStack(potionRingAdvanced, 1, i)));
         }
     }
 
---
+++ src/main/java/me/jacky1356400/actuallybaubles/item/ItemBatteryBauble.java
@@ -0,0 +1,38 @@
+package me.jacky1356400.actuallybaubles.item;
+
+import baubles.api.BaubleType;
+import baubles.api.IBauble;
+import de.ellpeck.actuallyadditions.mod.items.ItemBattery;
+import me.jacky1356400.actuallybaubles.ActuallyBaubles;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+
+public class ItemBatteryBauble extends ItemBattery implements IBauble {
+
+    public ItemBatteryBauble(String name, int capacity, int transfer){
+        super(name, capacity, transfer);
+        setCreativeTab(ActuallyBaubles.TAB);
+    }
+
+    @Override
+    public BaubleType getBaubleType(ItemStack itemStack) {
+        return BaubleType.TRINKET;
+    }
+
+    public boolean canEquip(ItemStack itemstack, EntityLivingBase player) {
+        return true;
+    }
+
+    public boolean canUnequip(ItemStack itemstack, EntityLivingBase player) {
+        return true;
+    }
+
+    public boolean willAutoSync(ItemStack itemstack, EntityLivingBase player) {
+        return false;
+    }
+
+    public void onWornTick(ItemStack stack, EntityLivingBase player) {
+        onUpdate(stack, player.getEntityWorld(), player, 0, false);
+    }
+
+}
